bad03acd55815efe5089257182d8448eabe5404a
Elaine MNkubitu <emm57@duke.edu>
2018-11-18 23:50:59 -0500

    Adding a service class for the seeker service

---
+++ src/app/eventManagent/eventPlanning/eventPlanning.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { EventPlanningComponent } from './eventPlanning.component';
+import { EventPlanningService } from './eventPlanning.service';
+
+@NgModule({
+  declarations: [
+    EventPlanningComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  providers: [EventPlanningService],
+  bootstrap: [EventPlanningComponent]
+})
+export class EventPlanningModule { }
---
+++ src/app/eventManagent/models/event.ts
@@ -0,0 +1,9 @@
+export class Event {
+    firstName: string;
+    lastName: string;
+    phoneNumber:number;
+    email:string;
+    date:Date;
+    location:string;
+    eventName:string;
+  }
