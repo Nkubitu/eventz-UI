f18b25c81ac4660472e95b7052c24a14fb3ab0e0
Elaine MNkubitu <emm57@duke.edu>
2019-02-10 23:50:06 -0500

    Adding some forms and more content to the eventPlanning component

---
+++ src/app/eventZ/login/login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: '../app/eventZ/eventPlanning/eventPlanning.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class LoginComponent {
+  
+}
